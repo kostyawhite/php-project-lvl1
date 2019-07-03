9112cc964f76a06802948043fb4340245bc7d68c
kostyawhite <kostyawhite89@gmail.com>
2019-07-03 13:16:10 +0300

    some error correction using phpcs

--- src/cli.php
+++ src/cli.php
@@ -4,8 +4,7 @@ use function \cli\line;
 
 function run()
 {
-  line("Welcome to the Brain Game\n");
-  $name = \cli\prompt('May I have your name?');
-  line("Hello, %s!", $name);
+    line("Welcome to the Brain Game\n");
+    $name = \cli\prompt('May I have your name?');
+    line("Hello, %s!", $name);
 }
-?>
