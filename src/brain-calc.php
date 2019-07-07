8deebf7bf1902cd9dc20c89f19a2cafa2681bafe
kostyawhite <kostyawhite89@gmail.com>
2019-07-07 18:57:01 +0300

    make brain-calc work with anonymous function

--- src/brain-calc.php
+++ src/brain-calc.php
@@ -1,8 +1,36 @@
 <?php
 
 namespace BrainGames\Calc;
+// namespace BrainGames\Cli;
 
 use function \cli\line;
 use function \cli\prompt;
 
-BrainGames\Cli\run();
+const TASK = "What is the result of the expression?";
+
+function run()
+{
+    $result = function() {
+        $operators = ['+', '-', '*'];
+        $operand1 = rand(1, 100);
+        $operand2 = rand(1, 100);
+        $result = 0;
+        $op = $operators[array_rand($operators, 1)];
+        switch ($op) {
+            case '+':
+                $result = $operand1 + $operand2;
+                break;
+
+            case '-':
+                $result = $operand1 - $operand2;
+                break;
+
+            case '*':
+                $result = $operand1 * $operand2;
+                break;
+        }
+        $question = "$operand1 $op $operand2";
+        return [$question, $result];
+    };
+    \BrainGames\Cli\run(TASK, $result);
+}
--- src/cli.php
+++ src/cli.php
@@ -4,10 +4,26 @@ namespace BrainGames\Cli;
 use function \cli\line;
 use function \cli\prompt;
 
-function run()
+function run($task, $result)
 {
     line("Welcome to the Brain Game");
-    // line($task);
+    line($task);
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
+    // number of trials
+    $correct = 3;
+    while ($correct !== 0) {
+        [$question, $rightAnswer] = $result();
+        line("Question: %s", $question);
+        $answer = prompt("Your asnwer");
+        if ($rightAnswer == $answer) {
+            line("Correct!");
+            // as per one correct answer decrease $correct counter
+            $correct -= 1;
+        } else {
+            line("'%s' is wrong answer ;(. Correct asnwer was '%s'.", $answer, $rightAnswer);
+            line("Let's try again, %s!", $name);
+        }
+    }
+    line("Congratulations, %s!", $name);
 }
